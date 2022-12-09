4379a59f063fd76d8fe0f54766d766fd18c90f9e
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-09 14:49:10 +0300

    Added loading and error display functionality to home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,14 @@
 import Feed from "./Feed";
 
-const Home = ({ posts }) => {
+const Home = ({ posts, isLoading, fetchError }) => {
   return (
     <main className="Home container">
-        {posts.length ? (
-          <Feed posts={posts} />
-        ) : (
-          <p style={{marginTop: '2rem'}}>No posts are available</p>
-        )}
+      {isLoading && <p className="mt-4">Requested posts are loading... </p>}
+      {!isLoading && fetchError && <p className="mt-4 text-danger">{fetchError}</p>}
+      {!isLoading && !fetchError && (posts.length ? 
+      <Feed posts={posts} />  : 
+        <p style={{marginTop: '2rem'}}>No posts are available</p>
+      )}
     </main>
   )
 }
